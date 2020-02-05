532ac860c7ebb40c3f4bab79927b4a4f7c5f5ff0
riddleb <42945456+riddleb@users.noreply.github.com>
2020-02-05 11:38:33 -0500

    Create create_record.php

---
+++ create_record.php
@@ -0,0 +1,9 @@
+<form method='post' action='insert_record.php'>
+
+First Name: <input name='fname' type='text' /><br>
+Last Name: <input name='lname' type='text' /><br>
+Phone Num: <input name='phone' type='text' /><br>
+
+<button type='submit'>Submit</Submit></button>
+
+</form>
